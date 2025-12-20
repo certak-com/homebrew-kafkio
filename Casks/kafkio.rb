62e949ed1afd68f1e8f80334c375a1ed6adf262b
Cormac Redmond <credmond85@gmail.com>
2025-12-20 01:17:23 +0000

    Update KafkIO download URL (fix), necessary due to bad actions workflow file

--- Casks/kafkio.rb
+++ Casks/kafkio.rb
@@ -2,7 +2,7 @@ cask "kafkio" do
   version "2.1.4"
   sha256 "5a7d7fc8208d89798984ab981a9000516af98dc544cc2ea97cdacedf6c6e55bc"
 
-  url "https://github.com/credmond/ct-ui-kafkio/releases/download/v2.1.4/KafkIO-macos-2.1.4-x64.pkg"
+  url "https://kafkio.com/download/kafkio/2.1.4/KafkIO-macos-2.1.4-x64.pkg"
   name "KafkIO"
   desc "Fast, easy Apache Kafka GUI for engineers and administrators"
   homepage "https://kafkio.com"
