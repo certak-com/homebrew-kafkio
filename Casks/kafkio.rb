55496bba808c125cf4ec7132fac50a2bd0646d8f
Cormac Redmond <credmond85@gmail.com>
2025-12-31 03:17:47 +0000

    Update KafkIO to version 2.1.8 (#4)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Casks/kafkio.rb
+++ Casks/kafkio.rb
@@ -1,8 +1,8 @@
 cask "kafkio" do
-  version "2.1.7"
-  sha256 "a2898947fd0130c4d3897874fb67b38d990ffa9621c98fa8bd13031ae48b3dd2"
+  version "2.1.8"
+  sha256 "a477fe4a71d9c27324a1ee666c4f76d4caaf18ee90710b5a9d0c20ea0bcbed53"
 
-  url "https://kafkio.com/download/kafkio/2.1.7/KafkIO-macos-2.1.7-x64.pkg"
+  url "https://kafkio.com/download/kafkio/2.1.8/KafkIO-macos-2.1.8-x64.pkg"
   name "KafkIO"
   desc "Fast, easy Apache Kafka GUI for engineers and administrators"
   homepage "https://kafkio.com"
