d2273796f90d75387bb2aa3679a2295539f12e6a
Cormac Redmond <credmond@certak.com>
2026-01-20 19:17:56 +0000

    Rollback KafkIO version to 2.1.8

--- Casks/kafkio.rb
+++ Casks/kafkio.rb
@@ -1,8 +1,8 @@
 cask "kafkio" do
-  version "2.1.9"
-  sha256 "525e80fe8f4e89c4ef2ca7b8cb094024249c8e8d923ad1322b6e743a6af82f2b"
+  version "2.1.8"
+  sha256 "a477fe4a71d9c27324a1ee666c4f76d4caaf18ee90710b5a9d0c20ea0bcbed53"
 
-  url "https://kafkio.com/download/kafkio/2.1.9/KafkIO-macos-2.1.9-x64.pkg"
+  url "https://kafkio.com/download/kafkio/2.1.8/KafkIO-macos-2.1.8-x64.pkg"
   name "KafkIO"
   desc "Fast, easy Apache Kafka GUI for engineers and administrators"
   homepage "https://kafkio.com"
