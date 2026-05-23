13c32c173f6e5ecfeff6e1f8385b037c83fb1a09
Cormac Redmond <credmond@certak.com>
2026-05-23 21:31:26 +0100

    Modify KafkIO cask to use DMG rather than PKG (which requires sudo)

--- Casks/kafkio.rb
+++ Casks/kafkio.rb
@@ -1,15 +1,13 @@
 cask "kafkio" do
   version "2.1.15"
-  sha256 "10fc5daada6b304327a1fe6885ab866f795f8419fba12df56a548933617d0dd6"
+  sha256 "a026fa82fc1b21ea9c947e6470df9254c459f0a5893b8749c924b0894a913db0"
 
-  url "https://kafkio.com/download/kafkio/2.1.15/KafkIO-macos-2.1.15-x64.pkg"
+  url "https://kafkio.com/download/kafkio/2.1.15/KafkIO-macos-2.1.15-x64.dmg"
   name "KafkIO"
   desc "Fast, easy Apache Kafka GUI for engineers and administrators"
   homepage "https://kafkio.com"
 
-  pkg "KafkIO-macos-#{version}-x64.pkg"
-
-  uninstall pkgutil: "com.certak.KafkIO*"
+  app "KafkIO.app"
 
   zap trash: [
     "~/Library/Application Support/kafkio",
