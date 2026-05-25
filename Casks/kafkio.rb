dccf7433ae0134a1d6d45c5aacef0a88f6a64fa7
Cormac Redmond <credmond@certak.com>
2026-05-25 20:11:40 +0100

    Update architecture for KafkIO to support arm64

--- Casks/kafkio.rb
+++ Casks/kafkio.rb
@@ -4,7 +4,7 @@ cask "kafkio" do
   app_name = "KafkIO"
   homepage_url = "https://kafkio.com"
 
-  arch arm: "x64", intel: "x64"
+  arch arm: "arm64", intel: "x64"
 
   sha256 arm:   "c48b0195fc2a9b807ac20e2a7b5964c3a1f2d477938898c82c4d5907011d853c", 
          intel: "c041c7f978e0c6f1b5e78e2f03120845229535bb47bc3f4d5bd7ff67de4326d0"
