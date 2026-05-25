da047799a3b16d872d25e600de063c09b3b23133
Cormac Redmond <credmond@certak.com>
2026-05-25 17:11:44 +0100

    First phase of Apple Silicone support, currently arm64 still gettind x64 build

--- Casks/kafkio.rb
+++ Casks/kafkio.rb
@@ -1,13 +1,30 @@
 cask "kafkio" do
   version "2.1.15"
-  sha256 "a026fa82fc1b21ea9c947e6470df9254c459f0a5893b8749c924b0894a913db0"
 
-  url "https://kafkio.com/download/kafkio/2.1.15/KafkIO-macos-2.1.15-x64.dmg"
-  name "KafkIO"
-  desc "Fast, easy Apache Kafka GUI for engineers and administrators"
-  homepage "https://kafkio.com"
+  app_name = "KafkIO"
+  homepage_url = "https://kafkio.com"
+
+  arch arm: "x64", intel: "x64"
+
+  sha256 arm:   "a026fa82fc1b21ea9c947e6470df9254c459f0a5893b8749c924b0894a913db0", 
+         intel: "a026fa82fc1b21ea9c947e6470df9254c459f0a5893b8749c924b0894a913db0"
+
+  url "https://kafkio.com/download/kafkio/#{version}/KafkIO-macos-#{version}-#{arch}.dmg"
+
+  name app_name
+  desc "The Fast, Easy Apache Kafka GUI for Engineers and Administrators"
+  homepage homepage_url
 
   app "KafkIO.app"
+
+  caveats do
+    <<~EOS
+      KafkIO has been installed into your Applications folder.
+
+      Launch it now with:
+        open /Applications/KafkIO.app
+    EOS
+  end
 
   zap trash: [
     "~/Library/Application Support/kafkio",
