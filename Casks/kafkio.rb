ffcfcc176bf76e41375f3bf1f202a479db619173
Cormac Redmond <credmond@certak.com>
2026-05-25 20:12:08 +0100

    Update KafkIO to version 2.1.16 (#12)
    * Update KafkIO to version 2.1.16
    
    * Update architecture for KafkIO to support arm64
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Casks/kafkio.rb
+++ Casks/kafkio.rb
@@ -1,13 +1,13 @@
 cask "kafkio" do
-  version "2.1.15"
+  version "2.1.16"
 
   app_name = "KafkIO"
   homepage_url = "https://kafkio.com"
 
-  arch arm: "x64", intel: "x64"
+  arch arm: "arm64", intel: "x64"
 
-  sha256 arm:   "a026fa82fc1b21ea9c947e6470df9254c459f0a5893b8749c924b0894a913db0", 
-         intel: "a026fa82fc1b21ea9c947e6470df9254c459f0a5893b8749c924b0894a913db0"
+  sha256 arm:   "c48b0195fc2a9b807ac20e2a7b5964c3a1f2d477938898c82c4d5907011d853c", 
+         intel: "c041c7f978e0c6f1b5e78e2f03120845229535bb47bc3f4d5bd7ff67de4326d0"
 
   url "https://kafkio.com/download/kafkio/#{version}/KafkIO-macos-#{version}-#{arch}.dmg"
 
